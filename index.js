bda5b9446d3d97011f485908f317d41e3f6cbfbf
Admonia <dancingshadows309@gmail.com>
2023-08-22 16:20:06 -0400

    solution code/ student style

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 // hello world route
@@ -21,35 +21,52 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets);
 });
 
+// we used this Link http://localhost:8080/api/v1/pets/owner?owner=John
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
-
+    const { owner } = req.query;
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    res.send(pet);
 });
+
+//We used this link http://localhost:8080/api/v1/pets/name?name=Spot
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
-
+    // console.log("name: " + req.query.pet.name);
+    const {name} = req.query;
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
+    res.send(pet)
 
 });
 
+
 app.listen(PORT, () => {
+
     console.log('Server is listening on port ' + PORT);
 });
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
